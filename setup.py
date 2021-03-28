f52f816bc087bab291dced5ed1539c826b7e855b
Alejandro Gálvez <>
2021-03-28 22:04:21 +0200

    Improved .gitignore, created package, requirements file and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='twitterMs',
+    version='0.1dev',
+    packages=['twitterms',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
