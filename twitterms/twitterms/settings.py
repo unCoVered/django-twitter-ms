811b098fd5a1434114a34c6067ff7840b072a51a
Alejandro Gálvez <>
2021-03-30 20:39:25 +0200

    Django getting started part4

--- twitterms/twitterms/settings.py
+++ twitterms/twitterms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tweetfetcher.apps.TweetfetcherConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
